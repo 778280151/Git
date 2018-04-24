4bbf6d6bad75b73fadbe25abe1064f2fb9e7837e
Mrs.ChongJie <Mrs.ChongJie@DESKTOP-7QIVKSU>
2018-04-24 10:52:19 +0800

    f

--- test.com Maven Webapp/src/main/java/dd.java
+++
@@ -1,8 +0,0 @@
-import org.apache.log4j.chainsaw.Main;
-
-public class dd {
-	public static void main(String[] args) {
-		son.getfather().getj().dfsf();
-		System.out.println("dd");
-	}
-}
--- test.com Maven Webapp/src/main/java/ddddd.java
+++
@@ -1,6 +0,0 @@
-
-public class ddddd {
-	public void dfsf() {
-		System.out.println("另外子类");
-	}
-}
